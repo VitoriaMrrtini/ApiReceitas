43cb52839cad1c64e0cc4a0322af1238bc763b65
Vitoria Martins Alves <139392804+VitoriaMrrtini@users.noreply.github.com>
2024-06-12 18:24:34 -0300

    Backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -108,15 +108,15 @@ export const getUsersBygenero = (req, res) => {
 }
 
 export const addUser = (req, res) => {
-    const sql = "INSERT INTO usuario (nome, senha, email, idade, genero) VALUES (?, ?, ?, ?, ?)";
+    const sql = "insert into usuario (nome, senha, email, idade, genero) values (?, ?, ?, ?, ?)";
 
-    const { nome, senha, email, idade, genero } = req.body;
+    const {nome, senha, email, idade, genero} = req.body;
 
     db.query(sql, [nome, senha, email, idade, genero], (err, data) => {
-        if (err) {
+        if(err){
             console.log("Erro ao processar a requisição.");
             return res.status(500).json(err);
-        } else {
+        }else{
             console.log("Usuário cadastrado com sucesso.");
             return res.status(201).json(data);
         }
@@ -153,5 +153,34 @@ export const deleteUser = (req, res) => {
             console.log("Usuário removido com sucesso.");
             return res.status(201).json(data);
         }
+    });
+}
+
+export const verificarcadastro = (req, res) => {
+
+    const {nome, senha, email, idade, genero} = req.body;
+    
+    db.query('SELECT * FROM usuario WHERE email = ?', [email], async(error, results) => {
+        if(error) {
+            console.error('Erro ao consultar o banco de dados: ', error);
+            return res.status(500).json({ message: 'Erro interno do servidor' });
+        }
+        
+        if (results.length > 0) {
+            console.error('o Email ja foi cadastrado, não entrou no BD');
+            return res.status(400).json({ message: 'Email já cadastrado' });
+        }
+    
+        const sql = "insert into usuario (nome, senha, email, idade, genero) values (?, ?, ?, ?, ?)";
+
+        db.query(sql, [nome, senha, email, idade, genero], (err, data) => {
+            if(err){
+                console.log("Erro ao processar a requisição.");
+                return res.status(500).json(err);
+            }else{
+                console.log("Usuário cadastrado com sucesso.");
+                return res.status(201).json(data);
+            }
+        });
     });
 }
